6ff1d214e5de73071eb66c751c2e25bf66b2a956
fz.bochkarev <H_y678ik>
2021-02-12 13:34:06 +0200

    ORM (Hibernate) и MVC Java pre-project. Практическая задача 2.2.1 v2 Transaction update

--- src/main/java/hiber/service/UserServiceImp.java
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -4,12 +4,10 @@ import hiber.dao.UserDao;
 import hiber.model.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.PersistenceException;
 import javax.persistence.Query;
 import java.util.List;
 
@@ -37,25 +35,15 @@ public class UserServiceImp implements UserService {
     @Override
     public User getUserByModelAndSeriesCar(String model, int series) {
         Session session = sessionFactory.openSession();
-        Transaction transaction = null;
         User user = null;
 
-        try {
-            transaction = session.beginTransaction();
-
-            String hql = "FROM User u WHERE u.car.model=:carModel " +
-                    "AND u.car.series=:carSeries";
-            Query query = session.createQuery(hql, User.class);
-            query.setParameter("carModel", model);
-            query.setParameter("carSeries", series);
-            List<User> users = ((org.hibernate.query.Query) query).list();
-            user = users.get(0);
-            transaction.commit();
-        } catch (IllegalStateException | PersistenceException e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-        }
+        String hql = "FROM User u WHERE u.car.model=:carModel " +
+                "AND u.car.series=:carSeries";
+        Query query = session.createQuery(hql, User.class);
+        query.setParameter("carModel", model);
+        query.setParameter("carSeries", series);
+        List<User> users = ((org.hibernate.query.Query) query).list();
+        user = users.get(0);
 
         session.close();
 
